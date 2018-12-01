5fbcffabcac129f095898edd945210131a8281b5
Faz <mfazal.q@gmail.com>
2018-12-01 14:50:32 +0800

    Update express

--- express-func/index.js
+++ express-func/index.js
@@ -2,6 +2,6 @@ const gcfExpressApp = require('gcf-express-app')
 const express = require('express')
 
 const app = express()
-app.get('/', (req, res) => res.send('Yup. I\'m alive.'))
+app.get('/', (req, res) => res.send('Hello. I\'m alive.'))
 
 exports.testExpressApp = gcfExpressApp(app)
